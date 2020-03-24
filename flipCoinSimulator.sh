87e4e51ca1b4835ecf5a6272879e3033e86e69e2
dvranga <62434972+dvranga@users.noreply.github.com>
2020-03-25 00:48:56 +0530

    Triplet Combination with Percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,59 +1,4 @@
 #Flip the Coin to Display Hs or Ts
-#declare -A Singlet
-echo enter value how many times to iterate the loop
-read number
-declare -A Singlet
-echo SingletCombination
-echo
-Singlet[Heads]=0;
-Singlet[Tails]=0;
-for (( i=0; i<$number; i++ ))
-
-do
-	Flip=$(( $RANDOM%2 ))
-	if [ $Flip -eq 1 ]
-	then
-		 Singlet[Heads]=$(((${Singlet[Heads]})+1));
-	else
-		 Singlet[Tails]=$(((${Singlet[Tails]})+1));
-	fi
-done
-for key in ${!Singlet[@]}
-do
-	Singlet[$key]=$(( (${Singlet[$key]} * 100) / $number));
-	echo "$key:${Singlet[$key]}"
-done
-
-echo DoubletCombination
-echo
-declare -A Doublet
-Doublet[HH]=0;
-Doublet[TH]=0
-Doublet[HT]=0;
-Doublet[TT]=0;
-for (( i=0; i<$number; i++ ))
-
-do
-	Flip=$(( $RANDOM%4 ))
-	if [ $Flip -eq 0 ]
-	then
-		 Doublet[HH]=$(((${Doublet[HH]})+1));
-	elif [ $Flip -eq 1 ]
-	then
-		Doublet[HT]=$(((${Doublet[HT]})+1));
-	elif [ $Flip -eq 2 ]
-	then
-		Doublet[TH]=$(((${Doublet[TH]})+1));
-	else
-		 Triplet[TT]=$(((${Doublet[TT]})+1));
-	fi
-done
-for key in ${!Doublet[@]}
-do
-	Doublet[$key]=$(( (${Doublet[$key]} * 100) / $number));
-	echo "$key:${Doublet[$key]}"
-done
-
 echo TripletCombination
 declare -A Triplet
 Triplet[HHH]=0;
@@ -99,8 +44,3 @@ do
 	Triplet[$key]=$(( (${Triplet[$key]} * 100) / $number));
 	echo "$key:${Triplet[$key]}"
 done
-array=($(for value in ${Triplet[@]}
-do
-	echo ${Triplet[$value]}
-done | sort ))
-echo ${array[@]}
