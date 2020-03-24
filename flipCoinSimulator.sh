1488d148170294b4466bbd469240b0d21051fadd
dvranga <devangmranganth>
2020-03-25 00:39:49 +0530

    Singlet Combination With Percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,26 +1,25 @@
-#Flip the Coin to Display Heads or Tails
+#Flip the Coin to Display Hs or Ts
 #declare -A Singlet
 echo enter value how many times to iterate the loop
 read number
-declare -A HashMap
-HashMap[head]=0;
-HashMap[tail]=0;
+declare -A Singlet
+echo SingletCombination
+echo
+Singlet[Heads]=0;
+Singlet[Tails]=0;
 for (( i=0; i<$number; i++ ))
 
 do
 	Flip=$(( $RANDOM%2 ))
 	if [ $Flip -eq 1 ]
 	then
-		 HashMap[head]=$(((${HashMap[head]})+1));
+		 Singlet[Heads]=$(((${Singlet[Heads]})+1));
 	else
-		 HashMap[tail]=$(((${HashMap[tail]})+1));
+		 Singlet[Tails]=$(((${Singlet[Tails]})+1));
 	fi
 done
-Headsoccure=${HashMap[head]};
-Tailsoccure=${HashMap[tail]};
-x=100
-headpercent=$(($(($Headsoccure * $x))/$number))
-tailpercent=$(($(($Tailsoccure * $x))/$number))
-echo headpercent=$headpercent
-echo tailpercent=$tailpercent
-
+for key in ${!Singlet[@]}
+do
+	Singlet[$key]=$(( (${Singlet[$key]} * 100) / $number));
+	echo "$key:${Singlet[$key]}"
+done
