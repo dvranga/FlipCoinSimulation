e7351397fa2e18e7c04cd4d3b342bbc87428208b
dvranga <devangmranganth>
2020-03-25 00:44:50 +0530

    Doublet Combination with Percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,42 +2,30 @@
 #declare -A Singlet
 echo enter value how many times to iterate the loop
 read number
-declare -A HashMap
-HashMap[headhead]=0;
-HashMap[tailhead]=0
-HashMap[headtail]=0;
-HashMap[tailtail]=0;
+declare -A Doublet
+Doublet[HH]=0;
+Doublet[TH]=0
+Doublet[HT]=0;
+Doublet[TT]=0;
 for (( i=0; i<$number; i++ ))
 
 do
 	Flip=$(( $RANDOM%4 ))
 	if [ $Flip -eq 0 ]
 	then
-		 HashMap[headhead]=$(((${HashMap[headhead]})+1));
+		 Doublet[HH]=$(((${Doublet[HH]})+1));
 	elif [ $Flip -eq 1 ]
 	then
-		HashMap[headtail]=$(((${HashMap[headtail]})+1));
+		Doublet[HT]=$(((${Doublet[HT]})+1));
 	elif [ $Flip -eq 2 ]
 	then
-		HashMap[tailhead]=$(((${HashMap[tailhead]})+1));
+		Doublet[TH]=$(((${Doublet[TH]})+1));
 	else
-		 HashMap[tailtail]=$(((${HashMap[tailtail]})+1));
+		 Triplet[TT]=$(((${Doublet[TT]})+1));
 	fi
 done
-HeadHeadoccure=${HashMap[headhead]};
-HeadTailoccure=${HashMap[headtail]};
-TailHeadoccure=${HashMap[tailhead]};
-TailTailoccure=${HashMap[tailtail]};
-echo HeadHeadoccure=${HashMap[headhead]};
-echo HeadTailoccure=${HashMap[headtail]};
-echo TailHeadoccure=${HashMap[tailhead]};
-echo TailTailoccure=${HashMap[tailtail]};
-x=100
-headheadpercent=$(($(($HeadHeadoccure * $x))/$number))
-headtailpercent=$(($(($HeadTailoccure * $x))/$number))
-tailheadpercent=$(($(($TailHeadoccure * $x))/$number))
-tailtailpercent=$(($(($TailTailoccure * $x))/$number))
-echo headheadpercent=$headheadpercent
-echo headtailpercent=$headtailpercent
-echo tailheadpercent=$tailheadpercent
-echo tailtailpercent=$tailtailpercent
+for key in ${!Doublet[@]}
+do
+	Doublet[$key]=$(( (${Doublet[$key]} * 100) / $number));
+	echo "$key:${Doublet[$key]}"
+done
